068523ed284f24d96a6a92a18e167a77f99be980
mondogenerate <137521730+mondogenerate@users.noreply.github.com>
2023-10-23 11:52:37 -0400

    Add files via upload

---
+++ Remote_Debloat.ps1
@@ -0,0 +1,23 @@
+﻿# Define the folder and file path
+$folderPath = "$HOME\tempDebloat"
+$file = "$folderPath\debloat.ps1" 
+$file2 = "$folderPath\Appslist.txt" 
+
+# Check if the folder exists
+if (-not (Test-Path $folderPath)) {
+    # Create the folder if it doesn't exist
+    New-Item -Path $folderPath -ItemType Directory
+}
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/mondogenerate/Scriptladdy/main/Win11Debloat.ps1 -OutFile $file -UseBasicParsing 
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/mondogenerate/Scriptladdy/main/Appslist.txt -OutFile $file2 -UseBasicParsing 
+
+
+ls $folderPath
+
+
+powershell -ep bypass $file -RemoveApps -Silent
+
+Start-Sleep 30
+
+if(test-path $folderPath){ Remove-Item $folderPath -Force }
